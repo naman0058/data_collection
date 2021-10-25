e73c8b1f45eda9298c9432dcac8d702476cd4bec
naman0058 <jnaman345@gmail.com>
2021-10-26 02:20:11 +0530

    a

--- routes/pool.js
+++ routes/pool.js
@@ -3,10 +3,10 @@ var mysql = require('mysql')
 require('dotenv').config()
 
 const pool = mysql.createPool({
- 
-    host : 'localhost',
-    user: 'root',
-    password : '123',
+  host:'167.71.231.201',
+  ///host : 'localhost',
+   user: 'root',
+  password:'123a@8Anmanraspaa123a@*Anmanraspaa',
     database: 'data_collection',
     port:'3306' ,
     multipleStatements: true
